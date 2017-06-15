6293b6ade726964ea9c1e417ca5d20878f96986b
zhang435 <zhang435@umail.iu.edu>
2017-06-15 15:02:14 -0400

    ?

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var links = document.querySelectorAll("a");
 for (index = 0; index < links.length; index++) {
-    console.log("apple");
     console.log(links[index].href);
     links[index].addEventListener("click", function(event) {
         window.open(this.href);
