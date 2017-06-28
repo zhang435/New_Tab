29e5f44b73f034aac81e0d1213c42dc2dde72318
zhang435 <zhang435@umail.iu.edu>
2017-06-27 22:42:36 -0400

    popup.html

--- remove_listener.js
+++ remove_listener.js
@@ -1,9 +1,6 @@
 /**
 // * this is a function that remove the added event
 **/
-
-
-
 var links = document.querySelectorAll("a");
 for (index = 0; index < links.length; index++) {
     links[index].removeEventListener("click", on_click,false)
